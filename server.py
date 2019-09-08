58f654d44fba01f25d53890c0577199ad986c5f5
fred fred <rassaifred@gmail.com>
2019-09-08 21:23:58 +0100

    start Edge services: Device, Server

--- server.py
+++ server.py
@@ -1,1 +1,23 @@
-print("welcome to the server")
+from threading import Thread
+
+
+class ServerEdge(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        # ----
+        print(self.__class__.__name__ + ":init")
+        # ----
+
+    def run(self):
+        # ----
+        print(self.__class__.__name__ + ":run")
+        # ----
+        print("welcome to the server")
+        # ----
+        while True:
+            pass
+
+
+if __name__ == '__main__':
+    serviceMain = ServerEdge()
+    serviceMain.start()
