c9bcb67279822ee6c4b42998388cc4258ae4702b
fred fred <rassaifred@gmail.com>
2019-10-16 19:07:06 +0100

    wip timer nocturne

--- pyedgeiotframework/southbound/PyHost.py
+++ pyedgeiotframework/southbound/PyHost.py
@@ -24,3 +24,14 @@ class PyHost(EdgeService):
 
     def power_off_host_callback(self, payload=None):
         print("Host power off")
+        import sys
+        if sys.platform == 'win32':
+
+            import ctypes
+            user32 = ctypes.WinDLL('user32')
+            user32.ExitWindowsEx(0x00000008, 0x00000000)
+
+        else:
+
+            import os
+            os.system('sudo shutdown now')
