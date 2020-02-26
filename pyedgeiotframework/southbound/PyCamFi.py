19f94a29f154e01bb01a04505e6b308ce156b292
fred fred <rassaifred@gmail.com>
2020-02-26 01:19:08 +0100

    init CamFiPy

---
+++ pyedgeiotframework/southbound/PyCamFi.py
@@ -0,0 +1,21 @@
+from PyEdgeIoTFramework.pyedgeiotframework.core.EdgeService import EdgeService
+
+
+class PyCamFi(EdgeService):
+
+    def __int__(self):
+        # ----
+        super().__init__()
+        # ----
+
+    def run(self):
+        # ----
+        super().run()
+        # ----
+
+
+class CamFiTool:
+    def scan_camfi(self):
+        pass
+    def get_camera_serial(self):
+        pass
